09ef5263e1533a9c9d26932d64879898eecfedf7
AlgorithmX2 <AlgorithmX2@gmail.com>
2016-01-23 20:12:14 -0600

    Minor fix for old build...

--- src/main/java/mod/chiselsandbits/render/GeneratedModelLoader.java
+++ src/main/java/mod/chiselsandbits/render/GeneratedModelLoader.java
@@ -75,7 +75,7 @@ public class GeneratedModelLoader implements ICustomModelLoader
 	public void onModelBakeEvent(
 			final ModelBakeEvent event )
 	{
-		ChiseledBlockSmartModel.resetCache();
+		ChisledBlockSmartModel.resetCache();
 		BitItemSmartModel.resetCache();
 
 		for ( final ModelResourceLocation rl : res )
