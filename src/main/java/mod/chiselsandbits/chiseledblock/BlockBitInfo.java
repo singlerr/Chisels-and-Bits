fd68519bed2c3fcfa8eb6061cba210340d4c9e66
AlgorithmX2 <AlgorithmX2@gmail.com>
2016-03-11 11:48:28 -0600

    Restrict more exotic blocks such as full slabs. Fixes #12

--- src/main/java/mod/chiselsandbits/chiseledblock/BlockBitInfo.java
+++ src/main/java/mod/chiselsandbits/chiseledblock/BlockBitInfo.java
@@ -2,6 +2,7 @@ package mod.chiselsandbits.chiseledblock;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.Random;
 
 import gnu.trove.map.TIntObjectMap;
 import gnu.trove.map.hash.TIntObjectHashMap;
@@ -11,14 +12,17 @@ import mod.chiselsandbits.core.ChiselsAndBits;
 import mod.chiselsandbits.core.Log;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockGlass;
+import net.minecraft.block.BlockGlowstone;
 import net.minecraft.block.BlockGrass;
 import net.minecraft.block.BlockIce;
 import net.minecraft.block.BlockSlime;
+import net.minecraft.block.BlockSnowBlock;
 import net.minecraft.block.BlockStainedGlass;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.Explosion;
@@ -153,6 +157,20 @@ public class BlockBitInfo
 			final ReflectionHelperBlock pb = new ReflectionHelperBlock();
 			final Class<? extends Block> blkClass = blk.getClass();
 
+			// custom dropping behavior?
+			pb.quantityDropped( null );
+			final Class<?> wc = blkClass.getMethod( pb.MethodName, Random.class ).getDeclaringClass();
+			final boolean quantityDroppedTest = wc == Block.class || wc == BlockGlowstone.class || wc == BlockStainedGlass.class || wc == BlockGlass.class || wc == BlockSnowBlock.class;
+
+			pb.quantityDroppedWithBonus( 0, null );
+			final boolean quantityDroppedWithBonusTest = blkClass.getMethod( pb.MethodName, int.class, Random.class ).getDeclaringClass() == Block.class || wc == BlockGlowstone.class;
+
+			pb.quantityDropped( null, 0, null );
+			final boolean quantityDropped2Test = blkClass.getMethod( pb.MethodName, IBlockState.class, int.class, Random.class ).getDeclaringClass() == Block.class;
+
+			final boolean isNotSlab = Item.getItemFromBlock( blk ) != null;
+			boolean itemExistsOrNotSpecialDrops = quantityDroppedTest && quantityDroppedWithBonusTest && quantityDropped2Test || isNotSlab;
+
 			// ignore blocks with custom collision.
 			pb.onEntityCollidedWithBlock( null, null, null );
 			final boolean entityCollisionTest = blkClass.getMethod( pb.MethodName, World.class, BlockPos.class, Entity.class ).getDeclaringClass() == Block.class || blkClass == BlockSlime.class;
@@ -178,9 +196,10 @@ public class BlockBitInfo
 				isFullBlock = true;
 				noCustomCollision = true;
 				hasBehavior = false;
+				itemExistsOrNotSpecialDrops = true;
 			}
 
-			if ( info.isCompatiable && noCustomCollision && info.hardness >= -0.01f && isFullBlock && supportedMaterial && !hasBehavior )
+			if ( info.isCompatiable && noCustomCollision && info.hardness >= -0.01f && isFullBlock && supportedMaterial && !hasBehavior && itemExistsOrNotSpecialDrops )
 			{
 				final boolean result = ChiselsAndBits.getConfig().isEnabled( blkClass.getName() );
 				supportedBlocks.put( blk, result );
