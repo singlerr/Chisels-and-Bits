95c9aa8e8cd4352bbd099e0d51bc0eecfcdbe9a6
AlgorithmX2 <AlgorithmX2@gmail.com>
2016-06-04 14:42:24 -0500

    Fixed derpy merge.

--- src/main/java/mod/chiselsandbits/chiseledblock/BlockBitInfo.java
+++ src/main/java/mod/chiselsandbits/chiseledblock/BlockBitInfo.java
@@ -1,6 +1,5 @@
 package mod.chiselsandbits.chiseledblock;
 
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -174,7 +173,7 @@ public class BlockBitInfo
 			final boolean quantityDroppedTest = wc == Block.class || wc == BlockGlowstone.class || wc == BlockStainedGlass.class || wc == BlockGlass.class || wc == BlockSnowBlock.class;
 
 			pb.quantityDroppedWithBonus( 0, null );
-			final boolean quantityDroppedWithBonusTest = getDeclaringClass( blkClass,pb.MethodName, int.class, Random.class ) == Block.class || wc == BlockGlowstone.class;
+			final boolean quantityDroppedWithBonusTest = getDeclaringClass( blkClass, pb.MethodName, int.class, Random.class ) == Block.class || wc == BlockGlowstone.class;
 
 			pb.quantityDropped( null, 0, null );
 			final boolean quantityDropped2Test = getDeclaringClass( blkClass, pb.MethodName, IBlockState.class, int.class, Random.class ) == Block.class;
@@ -187,7 +186,6 @@ public class BlockBitInfo
 			final boolean entityCollisionTest = blkClass.getMethod( pb.MethodName, World.class, BlockPos.class, Entity.class ).getDeclaringClass() == Block.class || blkClass == BlockSlime.class;
 
 			pb.onEntityCollidedWithBlock( null, null, null, null );
-			boolean noCustomCollision = getDeclaringClass( blkClass,pb.MethodName, World.class, BlockPos.class, IBlockState.class, Entity.class ) == Block.class || blkClass == BlockSlime.class;
 			final boolean entityCollision2Test = getDeclaringClass( blkClass, pb.MethodName, World.class, BlockPos.class, IBlockState.class, Entity.class ) == Block.class || blkClass == BlockSlime.class;
 
 			boolean noCustomCollision = entityCollisionTest && entityCollision2Test;
@@ -244,33 +242,33 @@ public class BlockBitInfo
 	}
 
 	private static Class<?> getDeclaringClass(
-			Class<?> blkClass,
-			String methodName,
-			Class<?>... args )
+			final Class<?> blkClass,
+			final String methodName,
+			final Class<?>... args )
 	{
 		try
 		{
-			blkClass.getDeclaredMethod( methodName, args);
+			blkClass.getDeclaredMethod( methodName, args );
 			return blkClass;
 		}
-		catch ( NoSuchMethodException e )
+		catch ( final NoSuchMethodException e )
 		{
 			// nothing here...
 		}
-		catch ( SecurityException e )
+		catch ( final SecurityException e )
 		{
 			// nothing here..
 		}
-		catch(NoClassDefFoundError e)
+		catch ( final NoClassDefFoundError e )
 		{
-			Log.info( "Unable to determine blocks eligibility for chiseling, " + blkClass.getName() +" attempted to load " + e.getMessage() );
+			Log.info( "Unable to determine blocks eligibility for chiseling, " + blkClass.getName() + " attempted to load " + e.getMessage() );
 			return blkClass;
 		}
-		catch(Throwable t )
-		{			
+		catch ( final Throwable t )
+		{
 			return blkClass;
 		}
-		
+
 		return getDeclaringClass(
 				blkClass.getSuperclass(),
 				methodName,
@@ -309,7 +307,7 @@ public class BlockBitInfo
 			final boolean test_b = getDeclaringClass( blkClass, reflectBlock.MethodName, IBlockState.class, EntityPlayer.class, World.class, BlockPos.class ) == Block.class;
 
 			reflectBlock.getExplosionResistance( null );
-			final Class exploResistanceClz = getDeclaringClass(blkClass, reflectBlock.MethodName, Entity.class );
+			final Class exploResistanceClz = getDeclaringClass( blkClass, reflectBlock.MethodName, Entity.class );
 			final boolean test_c = exploResistanceClz == Block.class;
 
 			reflectBlock.getExplosionResistance( null, null, null, null );
