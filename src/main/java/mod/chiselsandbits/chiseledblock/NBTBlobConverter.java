b5fc9d9b92763e0839ae5fa0a70a79fa9d3501d3
AlgorithmX2 <AlgorithmX2@gmail.com>
2016-08-05 14:57:49 -0500

    Cache Format details once acquired, should fix performance where items are constantly inspected. ( #116 )

--- src/main/java/mod/chiselsandbits/chiseledblock/NBTBlobConverter.java
+++ src/main/java/mod/chiselsandbits/chiseledblock/NBTBlobConverter.java
@@ -1,10 +1,7 @@
 package mod.chiselsandbits.chiseledblock;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.zip.InflaterInputStream;
 
-import io.netty.buffer.Unpooled;
 import mod.chiselsandbits.chiseledblock.data.VoxelBlob;
 import mod.chiselsandbits.chiseledblock.data.VoxelBlob.BlobStats;
 import mod.chiselsandbits.chiseledblock.data.VoxelBlobStateReference;
@@ -15,7 +12,6 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.PacketBuffer;
 
 public class NBTBlobConverter
 {
@@ -95,7 +91,7 @@ public class NBTBlobConverter
 		isNormalCube = tile.isNormalCube;
 		primaryBlockState = Block.getStateId( tile.getBlockState( Blocks.COBBLESTONE ) );
 		voxelBlobRef = tile.getBlobStateReference();
-		format = getFormat( voxelBlobRef );
+		format = voxelBlobRef == null ? -1 : voxelBlobRef.getFormat();
 	}
 
 	public void fillWith(
@@ -109,7 +105,7 @@ public class NBTBlobConverter
 			final VoxelBlob vb )
 	{
 		voxelBlobRef = new VoxelBlobStateReference( vb, 0 );
-		format = getFormat( voxelBlobRef );
+		format = voxelBlobRef.getFormat();
 		updateFromBlob();
 	}
 
@@ -170,34 +166,11 @@ public class NBTBlobConverter
 		}
 
 		voxelBlobRef = new VoxelBlobStateReference( v, 0 );
-		format = getFormat( voxelBlobRef );
+		format = voxelBlobRef.getFormat();
 
 		if ( tile != null )
 		{
 			tile.updateBlob( this, triggerUpdates );
-		}
-	}
-
-	private int getFormat(
-			final VoxelBlobStateReference v )
-	{
-		if ( v == null || v.getByteArray() == null || v.getByteArray().length == 0 )
-		{
-			return -1;
-		}
-
-		try
-		{
-			final InflaterInputStream arrayPeek = new InflaterInputStream( new ByteArrayInputStream( v.getByteArray() ) );
-			final byte[] peekBytes = new byte[5];
-			arrayPeek.read( peekBytes );
-
-			final PacketBuffer header = new PacketBuffer( Unpooled.wrappedBuffer( peekBytes ) );
-			return header.readVarIntFromBuffer();
-		}
-		catch ( final IOException e )
-		{
-			return 0;
 		}
 	}
 
