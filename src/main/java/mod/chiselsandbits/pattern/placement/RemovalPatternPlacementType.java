994c4a5ec2bfe8da9795c9629dd20bef6ad83938
Marc Hermans <marc.hermans@ldtteam.com>
2021-09-29 10:25:44 +0200

    Apply an additional fix to the removal pattern.

--- src/main/java/mod/chiselsandbits/pattern/placement/RemovalPatternPlacementType.java
+++ src/main/java/mod/chiselsandbits/pattern/placement/RemovalPatternPlacementType.java
@@ -46,7 +46,7 @@ public class RemovalPatternPlacementType extends ForgeRegistryEntry<IPatternPlac
     public VoxelShape buildVoxelShapeForWireframe(
       final IMultiStateSnapshot sourceSnapshot, final PlayerEntity player, final Vector3d targetedPoint, final Direction hitFace)
     {
-        final BlockPos targetedPosition = new BlockPos(targetedPoint).offset(hitFace.getOpposite().getNormal());
+        final BlockPos targetedPosition = hitFace.getAxisDirection() == Direction.AxisDirection.NEGATIVE ? new BlockPos(targetedPoint) : new BlockPos(targetedPoint).offset(hitFace.getOpposite().getNormal());
         final VoxelShape targetingShape = BlockPosStreamProvider.getForRange(
           player.isCrouching() ? targetedPoint : Vector3d.atLowerCornerOf(targetedPosition) ,
           player.isCrouching() ? targetedPoint.add(0.9999, 0.9999,0.9999): Vector3d.atLowerCornerOf(targetedPosition)
