7b5d565480323f648180287c8be72d8dc7395093
Marc Hermans <marc.hermans@ldtteam.com>
2021-09-29 11:03:50 +0200

    Fix batch processing prematurely turning the targeted block into a TE, even though it is not supported.

--- src/main/java/mod/chiselsandbits/multistate/mutator/ChiselAdaptingWorldMutator.java
+++ src/main/java/mod/chiselsandbits/multistate/mutator/ChiselAdaptingWorldMutator.java
@@ -511,6 +511,13 @@ public class ChiselAdaptingWorldMutator implements IWorldAreaMutator
             initializationState = Blocks.AIR.defaultBlockState();
         }
 
+        if (!IEligibilityManager.getInstance().canBeChiseled(currentState))
+        {
+            return () -> {
+                //Noop
+            };
+        }
+
         final Optional<Block> optionalWithConvertedBlock = IConversionManager.getInstance().getChiseledVariantOf(currentState);
         if (optionalWithConvertedBlock.isPresent())
         {
