fc02a96a02aa3f0ddf67d010230966c1f1e4c118
Marc Hermans <marc.hermans@ldtteam.com>
2021-09-30 21:52:39 +0200

    Started work on the off-thread implementation.

--- src/api/java/mod/chiselsandbits/api/neighborhood/IBlockNeighborhoodBuilder.java
+++ src/api/java/mod/chiselsandbits/api/neighborhood/IBlockNeighborhoodBuilder.java
@@ -1,9 +1,15 @@
 package mod.chiselsandbits.api.neighborhood;
 
 import mod.chiselsandbits.api.IChiselsAndBitsAPI;
+import mod.chiselsandbits.api.multistate.accessor.IAreaAccessor;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
 
 /**
  * Represents a system which can build unique block neighborhoods.
@@ -25,10 +31,13 @@ public interface IBlockNeighborhoodBuilder
     /**
      * Builds a block neighborhood for the requested target.
      *
-     * @param reader The block data reader to pull the neighborhood data from.
-     * @param target The target position.
+     * @param neighborhoodBlockStateProvider The blockstate provider to pull the neighborhood data from.
+     * @param neighborhoodAreaAccessorProvider The area accessor provider to pull the neighborhood data from.
      * @return A comparable an unique element targeting the requested position and containing the neighborhood data of the target position.
      */
     @NotNull
-    IBlockNeighborhood build(final IBlockReader reader, final BlockPos target);
+    IBlockNeighborhood build(
+            @Nullable Function<Direction, BlockState> neighborhoodBlockStateProvider,
+            @Nullable Function<Direction, IAreaAccessor> neighborhoodAreaAccessorProvider
+    );
 }
--- src/main/java/mod/chiselsandbits/neighborhood/BlockNeighborhoodBuilder.java
+++ src/main/java/mod/chiselsandbits/neighborhood/BlockNeighborhoodBuilder.java
@@ -1,57 +1,47 @@
 package mod.chiselsandbits.neighborhood;
 
-import mod.chiselsandbits.api.block.entity.IMultiStateBlockEntity;
+import mod.chiselsandbits.api.multistate.accessor.IAreaAccessor;
 import mod.chiselsandbits.api.neighborhood.IBlockNeighborhood;
 import mod.chiselsandbits.api.neighborhood.IBlockNeighborhoodBuilder;
 import mod.chiselsandbits.api.profiling.IProfilerSection;
 import mod.chiselsandbits.profiling.ProfilingManager;
 import net.minecraft.block.BlockState;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockReader;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.EnumMap;
+import java.util.function.Function;
 
-public final class BlockNeighborhoodBuilder implements IBlockNeighborhoodBuilder
-{
+public final class BlockNeighborhoodBuilder implements IBlockNeighborhoodBuilder {
     private static final BlockNeighborhoodBuilder INSTANCE = new BlockNeighborhoodBuilder();
 
-    public static BlockNeighborhoodBuilder getInstance()
-    {
+    public static BlockNeighborhoodBuilder getInstance() {
         return INSTANCE;
     }
 
-    private BlockNeighborhoodBuilder()
-    {
+    private BlockNeighborhoodBuilder() {
     }
 
     @Override
     public @NotNull IBlockNeighborhood build(
-      final IBlockReader reader, final BlockPos target)
-    {
+            @Nullable Function<Direction, BlockState> neighborhoodBlockStateProvider,
+            @Nullable Function<Direction, IAreaAccessor> neighborhoodAreaAccessorProvider
+    ) {
         final EnumMap<Direction, BlockNeighborhoodEntry> neighborhoodMap = new EnumMap<>(Direction.class);
 
-        try (IProfilerSection ignored2 = ProfilingManager.getInstance().withSection("Key building"))
-        {
-            if (reader != null && target != null)
-            {
-                for (final Direction value : Direction.values())
-                {
-                    final BlockPos offsetPos = target.relative(value);
-                    final BlockState state = reader.getBlockState(offsetPos);
-                    final TileEntity tileEntity = reader.getBlockEntity(offsetPos);
-                    if (!(tileEntity instanceof IMultiStateBlockEntity))
-                    {
+        try (IProfilerSection ignored2 = ProfilingManager.getInstance().withSection("Key building")) {
+            if (neighborhoodBlockStateProvider != null && neighborhoodAreaAccessorProvider != null) {
+                for (final Direction value : Direction.values()) {
+                    final BlockState state = neighborhoodBlockStateProvider.apply(value);
+                    final IAreaAccessor accessor = neighborhoodAreaAccessorProvider.apply(value);
+                    if (accessor == null) {
                         neighborhoodMap.put(value, new BlockNeighborhoodEntry(state));
-                    }
-                    else
-                    {
+                    } else {
                         neighborhoodMap.put(value, new BlockNeighborhoodEntry(
-                            state,
-                            ((IMultiStateBlockEntity) tileEntity).createNewShapeIdentifier()
-                          )
+                                        state,
+                                        accessor.createNewShapeIdentifier()
+                                )
                         );
                     }
                 }
