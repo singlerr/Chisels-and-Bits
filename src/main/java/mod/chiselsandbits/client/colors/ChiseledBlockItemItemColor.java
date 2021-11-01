3c6a60224728f12356626af20367f66a70ad8a43
Marc Hermans <marc.hermans@ldtteam.com>
2021-11-01 10:40:58 +0100

    Fix block item coloring crash

--- src/main/java/mod/chiselsandbits/client/colors/ChiseledBlockItemItemColor.java
+++ src/main/java/mod/chiselsandbits/client/colors/ChiseledBlockItemItemColor.java
@@ -1,7 +1,13 @@
 package mod.chiselsandbits.client.colors;
 
 import mod.chiselsandbits.api.block.state.id.IBlockStateIdManager;
+import mod.chiselsandbits.api.chiseling.eligibility.IEligibilityManager;
+import mod.chiselsandbits.item.bit.BitItem;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.world.item.AirItem;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.color.item.ItemColor;
@@ -20,10 +26,36 @@ public class ChiseledBlockItemItemColor implements ItemColor
       final int tint )
     {
         final BlockState state = IBlockStateIdManager.getInstance().getBlockStateFrom( tint >> TINT_BITS );
+        if(state.getBlock() instanceof LiquidBlock) {
+            return ((LiquidBlock) state.getBlock()).getFluid().getAttributes().getColor();
+        }
+
+        if ((!Minecraft.getInstance().options.keyShift.isUnbound() && Minecraft.getInstance().options.keyShift.isDown()) || (Minecraft.getInstance().getWindow() != null && Screen.hasShiftDown()))
+        {
+            final Block block = state.getBlock();
+            final Item item = block.asItem();
+            int tintValue = tint & TINT_MASK;
+
+            if (item != Items.AIR)
+            {
+                return Minecraft.getInstance().getItemColors().getColor(new ItemStack(item, 1), tintValue);
+            }
+
+            return 0xffffff;
+        }
+
+        if (!IEligibilityManager.getInstance().canBeChiseled(state))
+        {
+            return 0xffffff;
+        }
+
+        final ItemStack workingStack = new ItemStack(state.getBlock(), 1);
+        if (workingStack.getItem() instanceof AirItem)
+            return 0xffffff;
+
         final Block block = state.getBlock();
         final Item itemFromBlock = block.asItem();
         int tintValue = tint & TINT_MASK;
-
         return Minecraft.getInstance().getItemColors().getColor( new ItemStack(itemFromBlock, 1), tintValue );
     }
 }
