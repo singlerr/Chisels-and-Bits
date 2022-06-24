79efecc75ec1c92fb1f0832a4f0a94db05e88829
Phylogeny <molecularphylo@gmail.com>
2022-06-24 08:08:06 -0700

    Prevented the config builder's defineList method from returning a list of doubles that the compiler thinks are floats when the generic type passed is a float

--- platforms/forge/src/main/java/mod/chiselsandbits/forge/platform/configuration/ForgeDelegateConfigurationBuilder.java
+++ platforms/forge/src/main/java/mod/chiselsandbits/forge/platform/configuration/ForgeDelegateConfigurationBuilder.java
@@ -43,7 +43,12 @@ public class ForgeDelegateConfigurationBuilder implements IConfigurationBuilder
     {
         keys.add(key + ".comment");
         builder.comment(translateToLocal(key + ".comment"));
-        return builder.defineList(key, defaultValue, t -> true)::get;
+        if (containedType != Float.class)
+            return builder.defineList(key, defaultValue, t -> true)::get;
+
+        List<Double> defaultValueAsDoubles = defaultValue.stream().map(f -> ((Float) f).doubleValue()).toList();
+        ForgeConfigSpec.ConfigValue<List<? extends Double>> doubleListValue = builder.defineList(key, defaultValueAsDoubles, t -> t instanceof Double);
+        return () -> doubleListValue.get().stream().map(d -> containedType.cast(d.floatValue())).toList();
     }
 
     @Override
