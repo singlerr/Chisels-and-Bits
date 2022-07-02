80c15f98c3990596b094ac852518440835dea202
Marc Hermans <marc.hermans@ldtteam.com>
2022-07-02 22:22:46 +0200

    Delete Platforms directory

--- Platforms/Fabric/src/main/java/mod/chiselsandbits/fabric/platform/client/rendering/model/data/ModelDataKey.java
+++
@@ -1,7 +0,0 @@
-package mod.chiselsandbits.fabric.platform.client.rendering.model.data;
-
-import mod.chiselsandbits.platforms.core.client.models.data.IModelDataKey;
-
-public final class ModelDataKey<T> implements IModelDataKey<T>
-{
-}
--- Platforms/Forge/src/main/java/mod/chiselsandbits/forge/client/block/ForgeChiseledBlockRenderProperties.java
+++
@@ -1,8 +0,0 @@
-package mod.chiselsandbits.forge.client.block;
-
-import mod.chiselsandbits.client.block.ChiseledBlockRenderProperties;
-import net.minecraftforge.client.IBlockRenderProperties;
-
-public class ForgeChiseledBlockRenderProperties extends ChiseledBlockRenderProperties implements IBlockRenderProperties
-{
-}
