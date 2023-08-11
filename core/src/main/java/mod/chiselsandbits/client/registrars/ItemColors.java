5116c0be22cf0a1cfdb8ea4ef6b5fba0c7ab4830
Marc Hermans <marc.hermans@ldtteam.com>
2023-08-11 19:09:50 +0200

    Fix eligibility analysis Fix placement logic Fix client block and item colors

--- core/src/main/java/mod/chiselsandbits/client/registrars/ItemColors.java
+++ core/src/main/java/mod/chiselsandbits/client/registrars/ItemColors.java
@@ -23,6 +23,8 @@ public final class ItemColors
               configuration
                 .register(new ChiseledBlockItemItemColor(), ModItems.LEGACY_MATERIAL_CHISELED_BLOCKS.stream().map(IRegistryObject::get).toArray(ChiseledBlockItem[]::new));
               configuration
+                .register(new ChiseledBlockItemItemColor(), ModItems.CHISELED_BLOCK.get());
+              configuration
                 .register(new BitItemItemColor(), ModItems.ITEM_BLOCK_BIT.get());
               configuration
                 .register(new BitBagItemColor(), ModItems.ITEM_BIT_BAG_DEFAULT.get(), ModItems.ITEM_BIT_BAG_DYED.get());
