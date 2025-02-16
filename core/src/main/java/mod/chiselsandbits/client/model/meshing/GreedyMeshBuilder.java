0edc666587f1e8cc281b61a103adf839963c681f
Marc Hermans <marc.hermans@ldtteam.com>
2025-02-16 11:39:32 +0100

    Port to the new publishing and build system.

--- core/src/main/java/mod/chiselsandbits/client/model/meshing/GreedyMeshBuilder.java
+++ core/src/main/java/mod/chiselsandbits/client/model/meshing/GreedyMeshBuilder.java
@@ -1,20 +1,21 @@
 package mod.chiselsandbits.client.model.meshing;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import mod.chiselsandbits.api.blockinformation.BlockInformation;
 import mod.chiselsandbits.api.multistate.StateEntrySize;
-import mod.chiselsandbits.client.model.baked.chiseled.ChiselRenderType;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.Direction;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3f;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class GreedyMeshBuilder {
+
+    private static final int DIMENSIONS = 3;
 
     private GreedyMeshBuilder() {
         throw new IllegalStateException("Cannot instantiate utility class");
@@ -25,197 +26,172 @@ public class GreedyMeshBuilder {
         BlockInformation getMaterial(int x, int y, int z);
     }
 
-    public static GreedyMeshFace[] buildMesh(MaterialProvider materialData, ChiselRenderType chiselRenderType) {
+    public static GreedyMeshFace[] buildMesh(MaterialProvider data) {
         final List<GreedyMeshFace> faces = new ArrayList<>();
+        final int sizePerDimension = StateEntrySize.current().getBitsPerBlockSide();
+
         final Object2IntMap<BlockInformation> indexByMaterial = new Object2IntOpenHashMap<>();
-        final ArrayList<BlockInformation> materials = new ArrayList<>();
+        final Int2ObjectMap<BlockInformation> materialByIndex = new Int2ObjectOpenHashMap<>();
+        final int[] mask = new int[sizePerDimension * sizePerDimension]; // The mask is used to keep track of which faces have been added to the mesh.
 
         class MaterialProcessor {
             final int getMaterialIndex(int x, int y, int z) {
-                final BlockInformation blockInformation = materialData.getMaterial(x, y, z);
+                final BlockInformation blockInformation = data.getMaterial(x, y, z);
 
-                if (!chiselRenderType.isRequiredForRendering(blockInformation))
+                if (blockInformation.isAir())
                     return 0;
 
                 return indexByMaterial.computeIfAbsent(blockInformation, k -> {
                     int index = indexByMaterial.size() + 1;
-                    materials.add(blockInformation);
+                    materialByIndex.put(index, blockInformation);
                     return index;
                 });
             }
         }
+
         final MaterialProcessor materialProcessor = new MaterialProcessor();
 
-        final boolean renderTypeSolid = chiselRenderType.layer.equals(RenderType.solid());
-        final int bitsPerBlockSide = StateEntrySize.current().getBitsPerBlockSide();
-        final int bitsPerBlockSideP = bitsPerBlockSide + 2;
-        final int bitsPerBlockSideP2 = bitsPerBlockSideP * bitsPerBlockSideP;
+        for (int dimension = 0; dimension < DIMENSIONS; dimension++) {
+            int currentFirstDimensionIter, currentSecondDimensionIter;  // The current iteration position in the x, y, and z dimensions, also known as i, j, and k.
+            int width, height; //The current iteration dimensions in the x, y, and z dimensions, also known as l, w, and h.
 
-        final ArrayList<int[]> materialMaskCols = new ArrayList<>();
-        materialMaskCols.addFirst(new int[3 * bitsPerBlockSideP2]);
+            //We need to iterate over the other two dimensions.
+            final int firstDimensionIter = (dimension + 1) % DIMENSIONS; // The u and v dimensions are used to iterate over the other two dimensions.
+            final int secondDimensionIter = (dimension + 2) % DIMENSIONS;
 
-        for (int x = 0; x < bitsPerBlockSideP; x++) {
-            for (int y = 0; y < bitsPerBlockSideP; y++) {
-                for (int z = 0; z < bitsPerBlockSideP; z++) {
-                    if (!isSeeThrough(materialData.getMaterial(x - 1, y - 1, z - 1))) {
-                        materialMaskCols.getFirst()[x + (z * bitsPerBlockSideP)] |= (1 << y);
-                        materialMaskCols.getFirst()[x + (y * bitsPerBlockSideP) + bitsPerBlockSideP2] |= (1 << z);
-                        materialMaskCols.getFirst()[y + (z * bitsPerBlockSideP) + (bitsPerBlockSideP2 * 2)] |= (1 << x);
+            final int[] dimensionalIterator = new int[]{0, 0, 0};
+            final int[] calculatedDimensionsOffset = new int[]{0, 0, 0}; //Dimension offset for the already calculated dimensions.
+
+            calculatedDimensionsOffset[dimension] = 1;
+            for (dimensionalIterator[dimension] = -1; dimensionalIterator[dimension] < sizePerDimension; ) {
+
+                //Compute mask
+                int maskIndex = 0;
+                for (dimensionalIterator[secondDimensionIter] = 0; dimensionalIterator[secondDimensionIter] < sizePerDimension; ++dimensionalIterator[secondDimensionIter]) {
+                    for (dimensionalIterator[firstDimensionIter] = 0; dimensionalIterator[firstDimensionIter] < sizePerDimension; ++dimensionalIterator[firstDimensionIter], ++maskIndex) {
+
+                        int currentMaterial = (0 <= dimensionalIterator[dimension] ? materialProcessor.getMaterialIndex(
+                                dimensionalIterator[0],
+                                dimensionalIterator[1],
+                                dimensionalIterator[2]) : 0);
+                        int neighborMaterial = (dimensionalIterator[dimension] < sizePerDimension - 1 ? materialProcessor.getMaterialIndex(
+                                dimensionalIterator[0] + calculatedDimensionsOffset[0],
+                                dimensionalIterator[1] + calculatedDimensionsOffset[1],
+                                dimensionalIterator[2] + calculatedDimensionsOffset[2]) : 0);
+
+                        boolean aIsTruthy = currentMaterial != 0;
+                        boolean bIsTruthy = neighborMaterial != 0;
+
+                        if (aIsTruthy == bIsTruthy) {
+                            mask[maskIndex] = 0;
+                        } else if (aIsTruthy) {
+                            mask[maskIndex] = currentMaterial;
+                        } else {
+                            mask[maskIndex] = -neighborMaterial;
+                        }
                     }
-                    if (!renderTypeSolid) {
-                        int materialIndex = materialProcessor.getMaterialIndex(x - 1, y - 1, z - 1);
-                        if (materialIndex != 0) {
-                            while (materialIndex >= materialMaskCols.size())
-                                materialMaskCols.add(new int[3 * bitsPerBlockSideP2]);
-                            materialMaskCols.get(materialIndex)[x + (z * bitsPerBlockSideP)] |= (1 << y);
-                            materialMaskCols.get(materialIndex)[x + (y * bitsPerBlockSideP) + bitsPerBlockSideP2] |= (1 << z);
-                            materialMaskCols.get(materialIndex)[y + (z * bitsPerBlockSideP) + (bitsPerBlockSideP2 * 2)] |= (1 << x);
+                }
+
+                //Increment the computed position in the current dimension
+                dimensionalIterator[dimension]++;
+
+                //Compute the mesh
+                maskIndex = 0;
+                for (currentSecondDimensionIter = 0; currentSecondDimensionIter < sizePerDimension; ++currentSecondDimensionIter) {
+                    for(currentFirstDimensionIter = 0; currentFirstDimensionIter < sizePerDimension;) {
+                        int materialMask = mask[maskIndex];
+                        boolean isMaterialMaskTruthy = materialMask != 0;
+                        if (isMaterialMaskTruthy) {
+                            width = computeWidth(materialMask, mask, maskIndex, currentFirstDimensionIter, sizePerDimension);
+                            height = computeHeight(currentSecondDimensionIter, sizePerDimension, width, mask, maskIndex, materialMask);
+
+                            dimensionalIterator[firstDimensionIter] = currentFirstDimensionIter;
+                            dimensionalIterator[secondDimensionIter] = currentSecondDimensionIter;
+
+                            final GreedyMeshFace face = generateFace(materialMask, secondDimensionIter, height, firstDimensionIter, width, materialByIndex, dimensionalIterator, dimension, sizePerDimension);
+                            faces.add(face);
+
+                            clearMask(height, width, mask, maskIndex, sizePerDimension);
+
+                            currentFirstDimensionIter += width;
+                            maskIndex += width;
+                        } else {
+                            ++currentFirstDimensionIter;
+                            ++maskIndex;
                         }
                     }
                 }
             }
         }
 
-        List<HashMap<Integer, HashMap<Integer, int[]>>> faceData = new ArrayList<>(6);
-
-        if (renderTypeSolid) {
-            int[] faceMaskCols = new int[6 * bitsPerBlockSideP2];
-
-            for (int axis = 0; axis < 3; axis++) {
-                for (int i = 0; i < bitsPerBlockSideP2; i++) {
-                    int col = materialMaskCols.getFirst()[i + (bitsPerBlockSideP2 * axis)];
-                    faceMaskCols[i + (bitsPerBlockSideP2 * axis * 2)] = col & ~(col << 1);
-                    faceMaskCols[i + (bitsPerBlockSideP2 * ((axis * 2) + 1))] = col & ~(col >> 1);
-                }
-            }
-
-            for (int axis = 0; axis < 6; axis++) {
-                faceData.add(new HashMap<>());
-                for (int x = 0; x < bitsPerBlockSide; x++) {
-                    for (int z = 0; z < bitsPerBlockSide; z++) {
-                        int colIndex = 1 + x + ((z + 1) * bitsPerBlockSideP) + bitsPerBlockSideP2 * axis;
-
-                        int col = faceMaskCols[colIndex] >> 1;
-                        col &= ~(1 << bitsPerBlockSide);
-
-                        while (col != 0) {
-                            int y = Integer.numberOfTrailingZeros(col);
-                            col &= col - 1;
-
-                            int bitMaterial = materialProcessor.getMaterialIndex(
-                                    Direction.values()[axis].getAxis().choose(y, x, x),
-                                    Direction.values()[axis].getAxis().choose(x, y, z),
-                                    Direction.values()[axis].getAxis().choose(z, z, y)
-                            );
-
-                            faceData.get(axis)
-                                    .computeIfAbsent(bitMaterial, k -> new HashMap<>())
-                                    .computeIfAbsent(y, k -> new int[bitsPerBlockSide])[x] |= 1 << z;
-                        }
-                    }
-                }
-            }
-        }else {
-            final ArrayList<int[]> faceColMasks = new ArrayList<>();
-            materialMaskCols.forEach((material) -> faceColMasks.add(new int[6 * bitsPerBlockSideP2]));
-
-            for (int axis = 0; axis < 3; axis++) {
-                for (int i = 0; i < bitsPerBlockSideP2; i++) {
-                    int col = materialMaskCols.getFirst()[i + (bitsPerBlockSideP2 * axis)];
-                    for (int material = 1; material < faceColMasks.size(); material++) {
-                        int materialCol = materialMaskCols.get(material)[i + (bitsPerBlockSideP2 * axis)];
-                        faceColMasks.get(material)[i + (bitsPerBlockSideP2 * axis * 2)] = materialCol & ~((materialCol | col) << 1);
-                        faceColMasks.get(material)[i + (bitsPerBlockSideP2 * ((axis * 2) + 1))] = materialCol & ~((materialCol | col) >> 1);
-                    }
-                }
-            }
-
-            for (int axis = 0; axis < 6; axis++) {
-                faceData.add(new HashMap<>());
-                for (int x = 0; x < bitsPerBlockSide; x++) {
-                    for (int z = 0; z < bitsPerBlockSide; z++) {
-                        int colIndex = 1 + x + ((z + 1) * bitsPerBlockSideP) + bitsPerBlockSideP2 * axis;
-                        for (int material = 1; material < faceColMasks.size(); material++) {
-                            int col = faceColMasks.get(material)[colIndex] >> 1;
-                            col &= ~(1 << bitsPerBlockSide);
-
-                            while (col != 0) {
-                                int y = Integer.numberOfTrailingZeros(col);
-                                col &= col - 1;
-
-                                faceData.get(axis)
-                                        .computeIfAbsent(material, k -> new HashMap<>())
-                                        .computeIfAbsent(y, k -> new int[bitsPerBlockSide])[x] |= 1 << z;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        for (int axis = 0; axis < faceData.size(); axis++) {
-            Direction faceDirection = Direction.values()[axis];
-            faceData.get(axis).forEach((material, axisFaces) -> axisFaces.forEach(
-                    (axisPos, plane) -> faces.addAll(greedyMeshBinaryPlane(plane, materials.get(material - 1), faceDirection, axisPos, bitsPerBlockSide))
-            ));
-        }
         return faces.toArray(new GreedyMeshFace[0]);
     }
 
-    private static List<GreedyMeshFace> greedyMeshBinaryPlane(int[] data, BlockInformation material, Direction direction, int axisPos, final int BITS_PER_BLOCK_SIDE) {
-        List<GreedyMeshFace> axisFaces = new ArrayList<>();
-        for (int row = 0; row < data.length; row++) {
-            int y = 0;
-            while (y < BITS_PER_BLOCK_SIDE) {
-                y += Integer.numberOfTrailingZeros(data[row] >> y);
-                if (y >= BITS_PER_BLOCK_SIDE) continue;
-                int height = Integer.numberOfTrailingZeros(~data[row] >> y);
-
-                int hAsMask = height < 32 ? (1 << height) - 1 : ~0;
-                int mask = hAsMask << y;
-
-                int width = 1;
-                while (row + width < BITS_PER_BLOCK_SIDE) {
-                    int nextRowH = (data[row + width] >> y) & hAsMask;
-                    if (nextRowH != hAsMask) break;
-                    data[row + width] = data[row + width] & ~mask;
-                    width++;
-                }
-                axisFaces.add(generateFace(material, row, y, height, width, axisPos, direction, BITS_PER_BLOCK_SIDE));
-                y += height;
+    private static void clearMask(int height, int width, int[] mask, int maskIndex, int sizePerDimension) {
+        for (int x = 0; x < height; x++) {
+            for (int y = 0; y < width; y++) {
+                mask[maskIndex + y + x * sizePerDimension] = 0;
             }
         }
-        return axisFaces;
     }
 
     @NotNull
-    private static GreedyMeshFace generateFace(BlockInformation material, int x, int y, int height, int width, int axisPos, Direction direction, final int BITS_PER_BLOCK_SIDE) {
-        final Vector3f lowerLeft = transformPos(new Vector3f(x, axisPos, y), direction).div(BITS_PER_BLOCK_SIDE);
-        final Vector3f upperLeft = transformPos(new Vector3f(x, axisPos, y + height), direction).div(BITS_PER_BLOCK_SIDE);
-        final Vector3f lowerRight = transformPos(new Vector3f(x + width, axisPos, y), direction).div(BITS_PER_BLOCK_SIDE);
-        final Vector3f upperRight = transformPos(new Vector3f(x + width, axisPos, y + height), direction).div(BITS_PER_BLOCK_SIDE);
+    private static GreedyMeshFace generateFace(int materialMask, int secondDimensionIter, int height, int firstDimensionIter, int width, Int2ObjectMap<BlockInformation> materialByIndex, int[] dimensionalIterator, int dimension, float sizePerDimension) {
+        int[] deltaVertical = new int[]{0, 0, 0};
+        int[] deltaHorizontal = new int[]{0, 0, 0};
 
-        final boolean isEdge = axisPos == 0 || axisPos == BITS_PER_BLOCK_SIDE;
+        final Direction.Axis axis = Direction.Axis.values()[dimension];
 
-        return new GreedyMeshFace(material, lowerLeft, upperLeft, lowerRight, upperRight, direction, isEdge);
-    }
-
-    @SuppressWarnings("SuspiciousNameCombination")
-    private static Vector3f transformPos(Vector3f vector, Direction direction) {
-        Vector3f returnVector;
-        if (direction.getAxis().equals(Direction.Axis.X)) {
-            returnVector = new Vector3f(vector.y, vector.x, vector.z);
-        }else if (direction.getAxis().equals(Direction.Axis.Y)) {
-            returnVector = vector;
-        }else {
-            returnVector = new Vector3f(vector.x, vector.z, vector.y);
+        Direction.AxisDirection axisDirection = Direction.AxisDirection.POSITIVE;
+        if (materialMask > 0) {
+            deltaHorizontal[secondDimensionIter] = height;
+            deltaVertical[firstDimensionIter] = width;
+        } else {
+            axisDirection = Direction.AxisDirection.NEGATIVE;
+            materialMask = -materialMask;
+            deltaVertical[secondDimensionIter] = height;
+            deltaHorizontal[firstDimensionIter] = width;
         }
-        if (direction.getAxisDirection().equals(Direction.AxisDirection.POSITIVE))
-            returnVector.add(direction.getNormal().getX(), direction.getNormal().getY(), direction.getNormal().getZ());
-        return returnVector;
+
+        final Direction normalDirection = Direction.fromAxisAndDirection(axis, axisDirection);
+
+        final BlockInformation material = materialByIndex.get(materialMask);
+        final Vector3f lowerLeft = new Vector3f(dimensionalIterator[0], dimensionalIterator[1], dimensionalIterator[2]).div(sizePerDimension);
+        final Vector3f upperLeft = new Vector3f(dimensionalIterator[0] + deltaVertical[0], dimensionalIterator[1] + deltaVertical[1], dimensionalIterator[2] + deltaVertical[2]).div(sizePerDimension);
+        final Vector3f lowerRight = new Vector3f(dimensionalIterator[0] + deltaHorizontal[0], dimensionalIterator[1] + deltaHorizontal[1], dimensionalIterator[2] + deltaHorizontal[2]).div(sizePerDimension);
+        final Vector3f upperRight = new Vector3f(dimensionalIterator[0] + deltaVertical[0] + deltaHorizontal[0], dimensionalIterator[1] + deltaVertical[1] + deltaHorizontal[1], dimensionalIterator[2] + deltaVertical[2] + deltaHorizontal[2]).div(sizePerDimension);
+
+        final double axisValue = axis.choose(lowerLeft.x, lowerLeft.y, lowerLeft.z);
+
+        final boolean isEdge = axisValue == 0 || axisValue == sizePerDimension;
+
+        return new GreedyMeshFace(material, lowerLeft, upperLeft, lowerRight, upperRight, normalDirection, isEdge);
     }
 
-    private static boolean isSeeThrough(BlockInformation blockInformation) {
-        return blockInformation.isAir() || ChiselRenderType.TRANSLUCENT.isRequiredForRendering(blockInformation) || ChiselRenderType.TRANSLUCENT_FLUID.isRequiredForRendering(blockInformation);
+    private static int computeHeight(int j, int sizePerDimension, int width, int[] mask, int maskIndex, int materialMask) {
+        int height;
+        int k;
+        boolean done = false;
+        for(height = 1; j + height < sizePerDimension; height++) {
+            for(k = 0; k < width; k++) {
+                if(mask[maskIndex + k + height * sizePerDimension] != materialMask) {
+                    done = true;
+                    break;
+                }
+            }
+            if(done) {
+                break;
+            }
+        }
+        return height;
     }
+
+    private static int computeWidth(int materialMask, int[] mask, int maskIndex, int i, int sizePerDimension) {
+        int width = 1;
+        while (i + width < sizePerDimension && materialMask == mask[maskIndex + width]) {
+            width++;
+        }
+        return width;
+    }
+
 }
